7aaec28ce8172662d1928e7afb8aa259a8b709f0
samridhi-maharjan <samridhi.maharjan111@gmail.com>
2025-01-27 01:02:23 +0545

    nested array, fuction returning array,default values

--- script.js
+++ script.js
@@ -17,11 +17,35 @@ const restaurant = {
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    //ordering:fuction returns an array and we can destruct it
+    order: function(starterIndex, mainInbox){
+        return[this.starterMenu[starterIndex],this.mainMenu[mainInbox]];
+    }
 }
 
+//restaurant.order(2,0);      //result:Garlic Bread,pizza
+//console.log(restaurant.order(2,0 ));
 
-const [first, second]=restaurant.categories;
-console.log(first,second);              //gives first two data of categories from array named restaurant
+const[starter,mainCourse]=restaurant.order(2,0);        //same as maathi ko but in this way we can create two variable from one function call
+console.log(starter,mainCourse);
+
+//for nested array
+ const nested=[1,2,[3,4]];
+ const [i, ,j]=nested;
+ console.log(i,j);              //1,[3,4]
+ //to access individual:
+ const[k,,[l,m]]=nested;
+ console.log(k,l,m);
+
+ //setting default values
+// const [p,q,r]=[4,5];
+// console.log(p,q,r);             //result:4 5 undefined, as r is  not defined
+const [p=1,q=2,r=3]=[6,7];
+console.log(p,q,r);         //p=6,q=7,r=3; if you set a variable default value, the variable to its right must also have default value
+
+// const [first, second]=restaurant.categories;
+// console.log(first,second);              //gives first two data of categories from array named restaurant
 
 //we can skip an element by simply leaving a hole
 // const [first, ,second]=restaurant.categories;
@@ -29,11 +53,11 @@ console.log(first,second);              //gives first two data of categories fro
 
 
 //switching variables
-let [main,,secondary]=restaurant.categories;
-console.log(main,secondary);
-const temp=main;
-main=secondary;
-secondary=temp;
-console.log(main,secondary);
+// let [main,,secondary]=restaurant.categories;
+// console.log(main,secondary);
+// const temp=main;
+// main=secondary;
+// secondary=temp;
+// console.log(main,secondary);
 
 
